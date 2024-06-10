2d8c520b3528118e6c1314c02ad27259d423d649
elenabardho <elenabardho17@epoka.edu.al>
2024-06-10 16:32:04 +0100

    main

--- src/bot.py
+++ src/bot.py
@@ -2,29 +2,36 @@ import tweepy
 import requests
 import time
 from datetime import datetime
+import os
+from dotenv import load_dotenv
 
-# Repository owner and name
-REPO_OWNER = 'Ryun1'
-REPO_NAME = 'test-temp-repo'
+load_dotenv()
 
 # Twitter API credentials
-TWITTER_API_KEY = 'your_twitter_api_key'
-TWITTER_API_SECRET_KEY = 'your_twitter_api_secret_key'
-TWITTER_ACCESS_TOKEN = 'your_twitter_access_token'
-TWITTER_ACCESS_TOKEN_SECRET = 'your_twitter_access_token_secret'
+TWITTER_API_KEY = os.getenv("TWITTER_API_KEY")
+TWITTER_API_SECRET_KEY = os.getenv('TWITTER_API_SECRET_KEY')
+TWITTER_ACCESS_TOKEN = os.getenv('TWITTER_ACCESS_TOKEN')
+TWITTER_ACCESS_TOKEN_SECRET = os.getenv('TWITTER_ACCESS_TOKEN_SECRET')
+
+print(TWITTER_ACCESS_TOKEN,TWITTER_ACCESS_TOKEN_SECRET,TWITTER_API_KEY,TWITTER_API_SECRET_KEY)
 
 # GitHub API credentials
-GITHUB_TOKEN = 'your_github_token'
-REPO_OWNER = 'repository_owner'
-REPO_NAME = 'repository_name'
+GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
+REPO_OWNER =os.getenv('REPO_OWNER')
+REPO_NAME = os.getenv('REPO_NAME')
+
+print(GITHUB_TOKEN,REPO_NAME,REPO_OWNER)
 
 # Initialize Twitter API
-auth = tweepy.OAuthHandler(TWITTER_API_KEY, TWITTER_API_SECRET_KEY)
-auth.set_access_token(TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_TOKEN_SECRET)
-api = tweepy.API(auth)
+# auth = tweepy.OAuthHandler(TWITTER_API_KEY, TWITTER_API_SECRET_KEY)
+# auth.set_access_token(TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_TOKEN_SECRET)
+# api = tweepy.API(auth)
+
+client = tweepy.Client(consumer_key=TWITTER_API_KEY, consumer_secret=TWITTER_API_SECRET_KEY,access_token=TWITTER_ACCESS_TOKEN, access_token_secret=TWITTER_ACCESS_TOKEN_SECRET)
 
 # Track the latest PR seen
 latest_pr_id = None
+
 
 def get_latest_pr():
     url = f"https://api.github.com/repos/{REPO_OWNER}/{REPO_NAME}/pulls"
@@ -40,7 +47,14 @@ def tweet_new_pr(pr):
     pr_url = pr['html_url']
     pr_creator = pr['user']['login']
     tweet = f"New PR created by @{pr_creator}: {pr_title} {pr_url}"
-    api.update_status(tweet)
+
+    try:
+        # api.update_status(status=tweet)
+        client.create_tweet(text=tweet)
+    except tweepy.errors.TweepyException as e:
+        print(e)
+   
+    
 
 while True:
     latest_pr = get_latest_pr()
